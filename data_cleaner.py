244cdcd41691fb7a7453ee82cc7094cf1b38b3d6
AGATHA RABI <agatharabij@gmail.com>
2024-11-12 10:56:38 +0530

    blocks
    slowly working with pandas commands on the data
    
    Still slowly working on the data
    
    Customer data specific cleaning

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
+
 def clean_data(df, fill_values_dict):
+
     """
     Fumction to perform standard clean operations on the data.
 
@@ -17,11 +19,22 @@ def clean_data(df, fill_values_dict):
     # Fill the NA values
     df.fillna(value=fill_values_dict, inplace=True)
 
-
-
     # return the cleaned data
     return df
 
+def customer_data_clean(df, date_format, state_name):
 
+    """
 
+    :param customers_data:
+            is the data frame on which the following specific cleaning steps are done.
+    :param date_format:
+            the date format is not uniform. The format is to be made uniform.
+    :param state_name:
+            some state names have gaps others '-', replacing '-' with ' '.
+    :return:
+    """
+
+    df['Birthday'] = df['Birthday'].apply(lambda x: pd.to_datetime(x).strftime('%d/%m/%y'))
+    df['State'] = df['State'].str.replace('-', " ")
 
